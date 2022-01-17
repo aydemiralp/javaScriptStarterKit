2da7bf936836d3f914ff00b2915a55d4cbf22645
aydemiralp <alp.aydemir1@gmail.com>
2022-01-17 16:29:10 +0300

    17 Ocak Ders Sonu

--- src/app.js
+++ src/app.js
@@ -1,36 +1,13 @@
-console.log("Merhaba Kodlama.io")
-
-//JS type safe değildir
-let dolarBugun = 9.30
-
-let dolarDun = 9.20
-dolarDun = "9.20"
-{
-    let dolarDun = 9.10
-}
-
-console.log(dolarDun)
-
-const euroDun = 11.2
-//euroDun = 11
-
-console.log(euroDun)
 
 //array
-//camelCasing
-//PascalCasing
-let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
-
+// CamelCasing
+// PascalCasing
+let konutKredileri =["Konut Kredisi", "Emlak Konut Kredisi","Kamu Konut Kredisi"]
 console.log("<ul>")
-for(let i = 0;i<konutKredileri.length;i++){
-    console.log("<li>"+konutKredileri[i]+"</li>")
+for (let i = 0; i<konutKredileri.length ; i++) 
+{
+    console.log("<li>" + konutKredileri + "<li>")
+    
 }
-console.log("</ul>")
-
-{/* <ul>
-    <li>Konut kredisi</li>
-    <li>Emlak Konut Kredisi</li>
-    <li>Kamu Konut Kredisi</li>
-</ul> */}
-
-console.log(konutKredileri)
+console.log("<ul>")
+ //console.log(konutKredileri)
